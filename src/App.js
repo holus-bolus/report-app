1c307250cf8c64cc2d83c65db2fd5b5d4ceffea4
Oleksii <stinovoleksij@gmail.com>
2023-05-24 15:34:59 +0300

    Added components for splitting the code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import ExpenseItem from "./components/ExpenseItem";
+// import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
+
+import DisplayExpenses from "./components/DisplayExpenses/DisplayExpenses";
 
 function App() {
   const expenses = [
@@ -31,26 +33,7 @@ function App() {
     <div>
       <div className="container">
         <h2>Let's get started!</h2>
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        />
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        />
-        <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        />
-        <ExpenseItem
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-          date={expenses[3].date}
-        />
+        <DisplayExpenses items={expenses}/>
       </div>
     </div>
   );
---
+++ src/components/DisplayExpenses/DisplayExpenses.js
@@ -0,0 +1,28 @@
+import './DisplayExpenses.css';
+import ExpenseItem from "../ExpenseItem/ExpenseItem";
+
+function DisplayExpenses(props) {
+  return (
+    <div className="display-expenses">
+      <ExpenseItem
+        title={props.items[0].title}
+        expensesDone={props.items[0].amount}
+        date={props.items[0].date}/>
+      <ExpenseItem
+        title={props.items[1].title}
+        expensesDone={props.items[1].amount}
+        date={props.items[1].date}/>
+      <ExpenseItem
+        title={props.items[2].title}
+        expensesDone={props.items[2].amount}
+        date={props.items[2].date}/>
+      <ExpenseItem
+        title={props.items[3].title}
+        expensesDone={props.items[3].amount}
+        date={props.items[3].date}/>
+    </div>
+
+  )
+}
+
+export default DisplayExpenses;
