908d7d866ce36710e9d9c0bc60f8ddd0d655da10
Oleksii <stinovoleksij@gmail.com>
2023-05-28 18:49:53 +0300

    Addr the function components

--- src/components/Expenses/ExpenseItem/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem/ExpenseItem.js
@@ -2,15 +2,19 @@ import "./ExpenseItem.css";
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import Card from "../../UI/Card/Card";
 
-
 function ExpenseItem(props) {
+  let title = props.title;
+  const clickHandler = () => {
+    title = "Updated!";
+  };
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">$ {props.expensesDone}</div>
       </div>
+      <button onClick={clickHandler}>Change the title</button>
     </Card>
   );
 }
